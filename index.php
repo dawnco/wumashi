f021fe6352a4036edb54372d9292377ebde01ddc
dawnco <dawnco@163.com>
2014-06-03 16:20:49 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+define('ENV' , "development");
+define('CORE_PATH', dirname(__FILE__) . "/wumashi/"); //框架路径
+define('APP_PATH', dirname(__FILE__) . "/blog/"); //项目路径
+require CORE_PATH . "portal.php";
