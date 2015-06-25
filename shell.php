2a3508c1453799515d02a3ccc77ce0f8c8886ccc
dawnco <dawnco@163.com>
2015-06-25 20:52:03 +0800

    add const

--- shell.php
+++ shell.php
@@ -8,9 +8,19 @@ if (!defined('WUMASHI_PATH')) {
     define("WUMASHI_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR);
 }
 
-if (!defined("ROOT")) {
-    define("ROOT", dirname(WUMASHI_PATH) . DIRECTORY_SEPARATOR);
+if (!defined('ROOT')) {
+    define("ROOT", dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 }
+
+if (!defined('APP_PATH')) {
+    define("APP_PATH", ROOT . APP_NAME . DIRECTORY_SEPARATOR);
+}
+
+//第三方库代码
+if (!defined('VENDOR_PATH')) {
+    define("VENDOR_PATH", ROOT . "vendor" . DIRECTORY_SEPARATOR);
+}
+
 
 require WUMASHI_PATH . "run/init.php";
 require WUMASHI_PATH . "run/compatible.php";
